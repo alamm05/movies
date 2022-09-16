9ee7af760ba17e7a8f18543eb93ade330fd81441
Rashid Alam <alamm2412@gmail.com>
2022-09-16 22:55:11 +0530

    :construction:completed banner and navbar

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
+import {movies} from './getMovies'
 
 export default class Banner extends Component {
   render() {
+    // console.log(movies.results[0]);
+    let movie =movies.results[0];
     return (
-        <div className="card" >
-        <img src="..." className="card-img-top" alt="..."/>
+        <>
+        {movie =="" ?(
+            <div class="spinner-border text-danger" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+        <div className="card banner-card" >
+        <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">{movie.original_title}</h5>
+          <p className="card-text">{movie.overview}</p>
+          {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
         </div>
       </div>
-    )
+        )}
+      </>
+    );
   }
 }
