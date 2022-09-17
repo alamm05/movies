200ba5d6b88930d7712012e690b337a6906e64ad
Rashid Alam <alamm2412@gmail.com>
2022-09-18 00:51:22 +0530

    :fire:list to be completing

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {movies} from './getMovies'
 import axios from 'axios'
+import {API_KEY} from "../secrets"
 
 
 export default class List extends Component {
@@ -9,6 +10,9 @@ export default class List extends Component {
     console.log("constructor is called")
     this.state={
       hover:"",
+      parr:[1],
+      currPage:1,
+      movies:[],
     };
   }
 
@@ -25,17 +29,20 @@ export default class List extends Component {
   };
 async componentDidMount(){
   console.log("componentDidMount is called");
-  let res = await axios.get("https://api.themoviedb.org/3/movie/550?api_key=df993b768d2ad429b2c8a94fad44816a");
-  console.log(res.data);
- }
+  let res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=df993b768d2ad429b2c8a94fad44816a&language=en-US&page=${this.state.currPage}`);
+  // console.log(res.data);
+  this.setState({
+    movies:[...res.data.results],
+  });
+ };
 
   render() {
-    console.log("render is called")
-    let movie =movies.results;
+    // console.log("render is called")
+    // let movie =movies.results;
     return (
        <>
           {
-             movie.length ==0 ?
+             this.state.movies.length ==0 ?
              <div className="spinner-border text-info" role="status">
                 <span className="sr-only">Loading...</span>
            </div>:
@@ -45,7 +52,7 @@ async componentDidMount(){
                 <strong>Trending</strong>
               </h3>
               <div className='movies-list'>
-               {movie.map((movieObj)=>(
+               {this.state.movies.map((movieObj)=>(
                  
                  <div className="card movie-card" onMouseEnter={()=>this.handleEnter(movieObj.id)} onMouseLeave={this.handleLeave} >
                  <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`}className="card-img-top movie-img" alt="..."
@@ -69,8 +76,7 @@ async componentDidMount(){
               <ul class="pagination">
                 <li class="page-item"><a class="page-link" href="#">Previous</a></li>
                 <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
+              
                 <li class="page-item"><a class="page-link" href="#">Next</a></li>
               </ul>
               </nav>
