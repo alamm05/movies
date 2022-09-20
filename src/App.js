21b96c1bf6096c45972e1c4b8ea2569d98a6f569
Rashid Alam <alamm2412@gmail.com>
2022-09-20 18:57:30 +0530

    router-dom

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import List from './Components/List';
 import Favourites from './Components/Favourites';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import React from 'react';
 
 function App() {
   return (
-    <>
-       {/* <Navbar/>
-       <Banner/>
-       <List/> */}
+    <BrowserRouter>
        <Navbar/>
-       <Favourites/>
-    </>
+        <Routes>
+          <Route path='/' element={
+            <>
+            <Banner/>
+            <List/>
+            </>
+          }
+          />
+          <Route path='/fav' element={<Favourites/>}/>
+          </Routes>
+    </BrowserRouter>
   );
 }
 
